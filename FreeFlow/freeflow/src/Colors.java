105ea50005d93be2eb6f8076becb194017495c70
daan.hautekiet <daanhautekiet@gmail.com>
2021-03-16 18:47:58 +0100

    Added color codes

--- FreeFlow/freeflow/src/Colors.java
+++ FreeFlow/freeflow/src/Colors.java
@@ -2,7 +2,7 @@
  * Jonathan Dom 12/13/2020
  */
 public enum Colors {
-    RED("R"), GREEN("G"), YELLOW("Y"), BLUE("B");
+    RED("\033[0;31m"), GREEN("\033[0;32m"), YELLOW("\033[0;33m"), BLUE("\033[0;34m");
 
     private String color;
 
