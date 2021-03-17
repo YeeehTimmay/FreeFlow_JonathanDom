0393accd5e487b5c77af7da8a8405ed7494d91ee
daan.hautekiet <daanhautekiet@gmail.com>
2021-03-17 16:47:10 +0100

    Added createLevel4

--- FreeFlow/freeflow/src/Level.java
+++ FreeFlow/freeflow/src/Level.java
@@ -5,28 +5,35 @@ public class Level {
     private Board board;
     private int moves = 0;
 
-    public Level(LevelSizes size ) {
+    public Level(LevelSizes size) {
         createLevel(size);
 
     }
 
-    private void createLevel(LevelSizes size){
-            board = new Board(size.getLevelSize());
+    private void createLevel(LevelSizes size) {
+        board = new Board(size.getLevelSize());
 
-            switch (size){
-                case size4:
-            }
-
+        switch (size) {
+        case size4:
+            createLevel4(getRandomNumber(1, 2));
+            break;
+        }
 
     }
-    private int getRandomNumber(int min, int max){
-        return (int)(Math.random() * (max - min + 1) + min);
+
+    private void createLevel4(int level) {
+        switch (level) {
+        case 1:
+
+            break;
+        case 2:
+        
+            break;
+        }
     }
 
-
-
-
-
-
+    private int getRandomNumber(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
 
 }
