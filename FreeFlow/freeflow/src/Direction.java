0d55ba52f7c6c7b7a06960f5c9c5efb1fcf54b18
daan.hautekiet <daanhautekiet@gmail.com>
2021-03-17 20:48:23 +0100

    Removed NULL

--- FreeFlow/freeflow/src/Direction.java
+++ FreeFlow/freeflow/src/Direction.java
@@ -2,7 +2,7 @@
  * Jonathan Dom 1/10/2021
  */
 public enum Direction {
-    NORTH("North"), WEST("West"), SOUTH("South"), EAST("East"), NULL("Null");
+    NORTH("North"), WEST("West"), SOUTH("South"), EAST("East");
 
     private String direction;
 
