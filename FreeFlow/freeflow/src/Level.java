980a8185eda944dc827caada76e23ae973b232b0
daan.hautekiet <daanhautekiet@gmail.com>
2021-03-17 20:48:43 +0100

    IDK

--- FreeFlow/freeflow/src/Level.java
+++ FreeFlow/freeflow/src/Level.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Jonathan Dom 12/13/2020
  */
@@ -8,138 +10,207 @@ public class Level {
     public Level(LevelSizes size) {
         createLevel(size);
     }
-    public void showBoard(){
+
+    public void showBoard() {
         System.out.println(board);
     }
 
     private void createLevel(LevelSizes size) {
         board = new Board(size.getLevelSize());
         switch (size) {
-            case size5:
-                createLevel5(getRandomNumber(1, 2));
-                break;
-            case size6:
-                createLevel6(getRandomNumber(1, 2));
-                break;
-            case size7:
-                createLevel7(getRandomNumber(1, 2));
-                break;
+        case size5:
+            createLevel5(getRandomNumber(1, 2));
+            break;
+        case size6:
+            createLevel6(getRandomNumber(1, 2));
+            break;
+        case size7:
+            createLevel7(getRandomNumber(1, 2));
+            break;
         }
 
     }
-    public boolean makeMove(int x, int y, Direction direction){
-        if (board.getField(x,y).getType() == Type.EMPTY) return false;
 
-        if (!(board.checkValidField(x,y))){
-            System.out.println("That move is out of bounds. Try another. ");
-
+    public void makeMove() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("reeeeeeeeeee: ");
+            int moveX = scanner.nextInt();
+            int moveY = scanner.nextInt();
+            Direction direction = getDirection(scanner.next().charAt(0));
+            if (!(board.checkValidField(moveX, moveY)))
+                throw new IllegalArgumentException();
+            checkNextField(moveX, moveY, direction);
+            board.getField(moveX, moveY).setType(getMoveType(moveX, moveY, direction));
+        } catch (Exception e) {
+            makeMove();
         }
+    }
 
-        return true;
+    private Type getMoveType(int x, int y, Direction direction) {
+        switch (direction) {
+        case NORTH:
+            if ((x == 0 || x == board.getSize() - 1) && y == 0)
+                return Type.CORNER;
+            return Type.VERTICAL;
+        case EAST:
+            if ((y == 0 || y == board.getSize() - 1) && x == board.getSize() - 1)
+                return Type.CORNER;
+            return Type.HORIZONTAL;
+        case SOUTH:
+            if ((x == 0 || x == board.getSize() - 1) && y == board.getSize() - 1)
+                return Type.CORNER;
+            return Type.VERTICAL;
+        case WEST:
+            if ((y == 0 || y == board.getSize() - 1) && x == 0)
+                return Type.CORNER;
+            return Type.HORIZONTAL;
+        default:
+            return null;
+        }
+    }
+
+    private Direction getDirection(char charAt) {
+        switch (Character.toLowerCase(charAt)) {
+        case 'n':
+            return Direction.NORTH;
+        case 'e':
+            return Direction.EAST;
+        case 's':
+            return Direction.SOUTH;
+        case 'w':
+            return Direction.WEST;
+        default:
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkNextField(int x, int y, Direction direction) {
+        Field startField = board.getField(x, y);
+        Field stopField = null;
+        switch (direction) {
+        case NORTH:
+            stopField = board.getField(x, y - 1);
+            break;
+        case EAST:
+            stopField = board.getField(x + 1, y);
+            break;
+        case SOUTH:
+            stopField = board.getField(x, y + 1);
+            break;
+        case WEST:
+            stopField = board.getField(x - 1, y);
+            break;
+        }
+        if (stopField.getType() == Type.CIRCLE) {
+            if (startField.getColor() != stopField.getColor())
+                throw new IllegalArgumentException();
+        } else if (stopField.getType() != Type.EMPTY)
+            throw new IllegalArgumentException();
     }
 
     private void createLevel5(int level) {
         switch (level) {
-            case 1:
-                board.setField(0,0,Type.CIRCLE, Colors.RED);
-                board.setField(1,4,Type.CIRCLE, Colors.RED);
-                board.setField(2,0, Type.CIRCLE, Colors.GREEN);
-                board.setField(1,3, Type.CIRCLE, Colors.GREEN);
-                board.setField(2,1, Type.CIRCLE, Colors.BLUE);
-                board.setField(2,4, Type.CIRCLE, Colors.BLUE);
-                board.setField(4,0, Type.CIRCLE, Colors.YELLOW);
-                board.setField(3,3, Type.CIRCLE, Colors.YELLOW);
-                board.setField(4,1,Type.CIRCLE,Colors.PURPLE);
-                board.setField(3,4,Type.CIRCLE,Colors.PURPLE);
-                break;
-            case 2:
-                board.setField(0,4,Type.CIRCLE, Colors.RED);
-                board.setField(2,3,Type.CIRCLE, Colors.RED);
-                board.setField(2,2, Type.CIRCLE, Colors.GREEN);
-                board.setField(1,3, Type.CIRCLE, Colors.GREEN);
-                board.setField(0,3, Type.CIRCLE, Colors.BLUE);
-                board.setField(4,4, Type.CIRCLE, Colors.BLUE);
-                board.setField(0,0, Type.CIRCLE, Colors.YELLOW);
-                board.setField(4,3, Type.CIRCLE, Colors.YELLOW);
-                break;
-            default:
-                throw new IllegalArgumentException();
+        case 1:
+            board.setField(0, 0, Type.CIRCLE, Colors.RED);
+            board.setField(1, 4, Type.CIRCLE, Colors.RED);
+            board.setField(2, 0, Type.CIRCLE, Colors.GREEN);
+            board.setField(1, 3, Type.CIRCLE, Colors.GREEN);
+            board.setField(2, 1, Type.CIRCLE, Colors.BLUE);
+            board.setField(2, 4, Type.CIRCLE, Colors.BLUE);
+            board.setField(4, 0, Type.CIRCLE, Colors.YELLOW);
+            board.setField(3, 3, Type.CIRCLE, Colors.YELLOW);
+            board.setField(4, 1, Type.CIRCLE, Colors.PURPLE);
+            board.setField(3, 4, Type.CIRCLE, Colors.PURPLE);
+            break;
+        case 2:
+            board.setField(0, 4, Type.CIRCLE, Colors.RED);
+            board.setField(2, 3, Type.CIRCLE, Colors.RED);
+            board.setField(2, 2, Type.CIRCLE, Colors.GREEN);
+            board.setField(1, 3, Type.CIRCLE, Colors.GREEN);
+            board.setField(0, 3, Type.CIRCLE, Colors.BLUE);
+            board.setField(4, 4, Type.CIRCLE, Colors.BLUE);
+            board.setField(0, 0, Type.CIRCLE, Colors.YELLOW);
+            board.setField(4, 3, Type.CIRCLE, Colors.YELLOW);
+            break;
+        default:
+            throw new IllegalArgumentException();
         }
     }
 
     private void createLevel6(int level) {
         switch (level) {
-            case 1:
-                board.setField(4,0,Type.CIRCLE, Colors.RED);
-                board.setField(2,3,Type.CIRCLE, Colors.RED);
-                board.setField(0,0, Type.CIRCLE, Colors.GREEN);
-                board.setField(0,4, Type.CIRCLE, Colors.GREEN);
-                board.setField(0,5, Type.CIRCLE, Colors.BLUE);
-                board.setField(2,5, Type.CIRCLE, Colors.BLUE);
-                board.setField(1,0, Type.CIRCLE, Colors.YELLOW);
-                board.setField(0,5, Type.CIRCLE, Colors.YELLOW);
-                board.setField(4,1, Type.CIRCLE, Colors.PURPLE);
-                board.setField(2,4, Type.CIRCLE, Colors.PURPLE);
-                board.setField(2,0, Type.CIRCLE, Colors.CYAN);
-                board.setField(2,2, Type.CIRCLE, Colors.CYAN);
+        case 1:
+            board.setField(4, 0, Type.CIRCLE, Colors.RED);
+            board.setField(2, 3, Type.CIRCLE, Colors.RED);
+            board.setField(0, 0, Type.CIRCLE, Colors.GREEN);
+            board.setField(0, 4, Type.CIRCLE, Colors.GREEN);
+            board.setField(0, 5, Type.CIRCLE, Colors.BLUE);
+            board.setField(2, 5, Type.CIRCLE, Colors.BLUE);
+            board.setField(1, 0, Type.CIRCLE, Colors.YELLOW);
+            board.setField(0, 5, Type.CIRCLE, Colors.YELLOW);
+            board.setField(4, 1, Type.CIRCLE, Colors.PURPLE);
+            board.setField(2, 4, Type.CIRCLE, Colors.PURPLE);
+            board.setField(2, 0, Type.CIRCLE, Colors.CYAN);
+            board.setField(2, 2, Type.CIRCLE, Colors.CYAN);
 
-                break;
-            case 2:
-                board.setField(3,2,Type.CIRCLE, Colors.RED);
-                board.setField(1,4,Type.CIRCLE, Colors.RED);
-                board.setField(5,0, Type.CIRCLE, Colors.GREEN);
-                board.setField(5,2, Type.CIRCLE, Colors.GREEN);
-                board.setField(2,0, Type.CIRCLE, Colors.BLUE);
-                board.setField(5,3, Type.CIRCLE, Colors.BLUE);
-                board.setField(2,2, Type.CIRCLE, Colors.YELLOW);
-                board.setField(2,4, Type.CIRCLE, Colors.YELLOW);
-                board.setField(1,0, Type.CIRCLE, Colors.PURPLE);
-                board.setField(5,4, Type.CIRCLE, Colors.PURPLE);
-                board.setField(3,3, Type.CIRCLE, Colors.CYAN);
-                board.setField(4,4, Type.CIRCLE, Colors.CYAN);
+            break;
+        case 2:
+            board.setField(3, 2, Type.CIRCLE, Colors.RED);
+            board.setField(1, 4, Type.CIRCLE, Colors.RED);
+            board.setField(5, 0, Type.CIRCLE, Colors.GREEN);
+            board.setField(5, 2, Type.CIRCLE, Colors.GREEN);
+            board.setField(2, 0, Type.CIRCLE, Colors.BLUE);
+            board.setField(5, 3, Type.CIRCLE, Colors.BLUE);
+            board.setField(2, 2, Type.CIRCLE, Colors.YELLOW);
+            board.setField(2, 4, Type.CIRCLE, Colors.YELLOW);
+            board.setField(1, 0, Type.CIRCLE, Colors.PURPLE);
+            board.setField(5, 4, Type.CIRCLE, Colors.PURPLE);
+            board.setField(3, 3, Type.CIRCLE, Colors.CYAN);
+            board.setField(4, 4, Type.CIRCLE, Colors.CYAN);
 
-                break;
-            default:
-                throw new IllegalArgumentException();
+            break;
+        default:
+            throw new IllegalArgumentException();
         }
     }
 
     private void createLevel7(int level) {
         switch (level) {
-            case 1:
-                board.setField(6,1,Type.CIRCLE, Colors.RED);
-                board.setField(4,5,Type.CIRCLE, Colors.RED);
-                board.setField(3,3, Type.CIRCLE, Colors.GREEN);
-                board.setField(2,4, Type.CIRCLE, Colors.GREEN);
-                board.setField(6,0, Type.CIRCLE, Colors.BLUE);
-                board.setField(5,6, Type.CIRCLE, Colors.BLUE);
-                board.setField(4,4, Type.CIRCLE, Colors.YELLOW);
-                board.setField(5,5, Type.CIRCLE, Colors.YELLOW);
-                board.setField(1,2, Type.CIRCLE, Colors.PURPLE);
-                board.setField(5,1, Type.CIRCLE, Colors.PURPLE);
-                board.setField(4,3, Type.CIRCLE, Colors.CYAN);
-                board.setField(6,6, Type.CIRCLE, Colors.CYAN);
+        case 1:
+            board.setField(6, 1, Type.CIRCLE, Colors.RED);
+            board.setField(4, 5, Type.CIRCLE, Colors.RED);
+            board.setField(3, 3, Type.CIRCLE, Colors.GREEN);
+            board.setField(2, 4, Type.CIRCLE, Colors.GREEN);
+            board.setField(6, 0, Type.CIRCLE, Colors.BLUE);
+            board.setField(5, 6, Type.CIRCLE, Colors.BLUE);
+            board.setField(4, 4, Type.CIRCLE, Colors.YELLOW);
+            board.setField(5, 5, Type.CIRCLE, Colors.YELLOW);
+            board.setField(1, 2, Type.CIRCLE, Colors.PURPLE);
+            board.setField(5, 1, Type.CIRCLE, Colors.PURPLE);
+            board.setField(4, 3, Type.CIRCLE, Colors.CYAN);
+            board.setField(6, 6, Type.CIRCLE, Colors.CYAN);
 
-                break;
-            case 2:
-                board.setField(2,0,Type.CIRCLE, Colors.RED);
-                board.setField(3,6,Type.CIRCLE, Colors.RED);
-                board.setField(2,2, Type.CIRCLE, Colors.GREEN);
-                board.setField(5,3, Type.CIRCLE, Colors.GREEN);
-                board.setField(3,1, Type.CIRCLE, Colors.BLUE);
-                board.setField(5,1, Type.CIRCLE, Colors.BLUE);
-                board.setField(3,3, Type.CIRCLE, Colors.YELLOW);
-                board.setField(4,4, Type.CIRCLE, Colors.YELLOW);
-                board.setField(2,1, Type.CIRCLE, Colors.PURPLE);
-                board.setField(2,5, Type.CIRCLE, Colors.PURPLE);
-                board.setField(2,3, Type.CIRCLE, Colors.CYAN);
-                board.setField(5,4, Type.CIRCLE, Colors.CYAN);
-                board.setField(1,0, Type.CIRCLE, Colors.WHITE);
-                board.setField(2,6, Type.CIRCLE, Colors.WHITE);
+            break;
+        case 2:
+            board.setField(2, 0, Type.CIRCLE, Colors.RED);
+            board.setField(3, 6, Type.CIRCLE, Colors.RED);
+            board.setField(2, 2, Type.CIRCLE, Colors.GREEN);
+            board.setField(5, 3, Type.CIRCLE, Colors.GREEN);
+            board.setField(3, 1, Type.CIRCLE, Colors.BLUE);
+            board.setField(5, 1, Type.CIRCLE, Colors.BLUE);
+            board.setField(3, 3, Type.CIRCLE, Colors.YELLOW);
+            board.setField(4, 4, Type.CIRCLE, Colors.YELLOW);
+            board.setField(2, 1, Type.CIRCLE, Colors.PURPLE);
+            board.setField(2, 5, Type.CIRCLE, Colors.PURPLE);
+            board.setField(2, 3, Type.CIRCLE, Colors.CYAN);
+            board.setField(5, 4, Type.CIRCLE, Colors.CYAN);
+            board.setField(1, 0, Type.CIRCLE, Colors.WHITE);
+            board.setField(2, 6, Type.CIRCLE, Colors.WHITE);
 
-                break;
-            default:
-                throw new IllegalArgumentException();
+            break;
+        default:
+            throw new IllegalArgumentException();
 
         }
     }
