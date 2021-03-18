d4e3d6027aec846f5dfa019963442052c81062d5
Yeeeh_Timmay <jonathan.dom022001@gmail.com>
2021-03-18 15:35:45 +0100

    ree

--- FreeFlow/freeflow/src/PlayGame.java
+++ FreeFlow/freeflow/src/PlayGame.java
@@ -3,7 +3,7 @@
  */
 public class PlayGame {
     public static void main(String[] args) {
-        Player player = new Player("A");
+        Player player = new Player(IO.promptForText("Welcome! Enter your name here"));
         Level level = new Level(IO.promptForLevelSize());
         while (!level.checkIfWon()) {
             level.showBoard();
